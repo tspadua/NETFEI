019c0636dd1f431fb62f2473e64148fee7655036
bbrigideNess <bbrigide@ness.com.br>
2021-10-27 22:18:35 -0300

    started home page

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,32 @@
+import React, { useState, userEffect, useEffect } from 'react';
+import { useHistory } from 'react-router';
+
+import './styles.css';
+import Header from '../../components/Header';
+import Thumbnail from '../../components/Thumbnail';
+
+import thumbnail from '../../assets/thumbnail.png';
+
+import api from '../../services/api';
+
+export default function Home() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        api.get('index').then(response => {
+            setVideos(response.data)
+        })
+    }, []);
+
+    return (
+        <div>
+            <Header />
+            <ul>
+                {videos.map(video => (
+                    <Thumbnail image={thumbnail} 
+                               title={video.title}/>
+                ))}
+            </ul>
+        </div>
+    );
+}
